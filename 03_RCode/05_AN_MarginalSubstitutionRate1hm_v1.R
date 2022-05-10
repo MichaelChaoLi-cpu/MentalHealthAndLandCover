8abf4f4b017023ffe5a9b210dea7111ef9226392
Chao Li <chaoli0394@gmail.com>
2022-05-10 11:12:22 +0900

    A: add 0510 version of MSR; reivse R 5_AN; done

--- 03_RCode/05_AN_MarginalSubstitutionRate1hm_v1.R
+++ 03_RCode/05_AN_MarginalSubstitutionRate1hm_v1.R
@@ -6,6 +6,9 @@ library(dplyr)
 library(tidyverse)
 library(ggplot2)
 
+load("04_Results/04_pdp_refit_weights_rf47weighted.RData")
+load("04_Results/01_RFresult_47var_weighted.RData")
+
 #### Marginal Substitute Rate functions:
 land.MSR.estimation <- function(land_value, inc_value, input_land_model, 
                                 input_inc_model = pred.line.di_inc[[2]]){
@@ -37,16 +40,24 @@ land.MSR.estimation <- function(land_value, inc_value, input_land_model,
   g_Inc <- predict(input_inc_model, x)
   g_Inc_delta <- g_Inc + f_LA_delta - f_LA
   income_change <- c()
-  for(single_g_Inc_delta in g_Inc_delta){
-    income_change_single <- try(calculate_root(single_g_Inc_delta, input_inc_model), silent = TRUE)
+  g_Inc_delta <- g_Inc_delta %>% as.vector()
+  inc_value <- inc_value[[1]] %>% as.vector()
+  loop_time <-  1
+  while(loop_time < (length(g_Inc_delta) + 1)){
+    single_g_Inc_delta <- g_Inc_delta[loop_time]
+    people_income <- inc_value[loop_time]
+    income_change_single <- try(calculate_root(single_g_Inc_delta, people_income = people_income,
+                                                 input_inc_model), silent = TRUE)
     if(!inherits(income_change_single, "try-error")){
-      income_change_single <- calculate_root(single_g_Inc_delta, input_inc_model)
+      income_change_single <- calculate_root(single_g_Inc_delta, people_income = people_income,
+                                             input_inc_model)
     } else {
       income_change_single <- NA
     }
     income_change <- append(income_change, income_change_single)
+    loop_time <- loop_time + 1
   }
-  #income_change <- income_change - inc_value
+  income_change <- income_change - inc_value
   return(income_change)
 }
 
@@ -62,9 +73,9 @@ f <- function(x, coef, b){
   return(value_function)
 }
 
-calculate_root <- function(est_MH_inc, input_inc_model = input_inc_model){
+calculate_root <- function(est_MH_inc, people_income, input_inc_model = input_inc_model){
   coef <- coefficients(input_inc_model)
-  root <- uniroot(f, c(-1, 1), coef = coef, b = est_MH_inc, tol = 0.0001)
+  root <- uniroot(f, c(people_income-0.05, people_income+0.05), coef = coef, b = est_MH_inc, tol = 0.0001)
   return(root$root)
 }
 
@@ -82,57 +93,52 @@ data_47_MSR$MSR_crop <- land.MSR.estimation(data_47_MSR$crop2015, data_47_MSR$di
                                             pred.line.crop2015[[2]], pred.line.di_inc[[2]])
 summary(data_47_MSR$MSR_crop)
 ggplot(data = data_47_MSR) + 
-  geom_histogram(aes(x = MSR_crop), bins = 100) + 
-  xlim(-1, 1)
+  geom_histogram(aes(x = MSR_crop), bins = 100) 
 
 data_47_MSR$MSR_fore <- land.MSR.estimation(data_47_MSR$fore2015, data_47_MSR$di_inc_gdp,
                                             pred.line.fore2015[[2]], pred.line.di_inc[[2]])
 summary(data_47_MSR$MSR_fore)
 ggplot(data = data_47_MSR) + 
-  geom_histogram(aes(x = MSR_fore), bins = 100) + 
-  xlim(-1, 1)
+  geom_histogram(aes(x = MSR_fore), bins = 100) 
 
 data_47_MSR$MSR_gras <- land.MSR.estimation(data_47_MSR$gras2015, data_47_MSR$di_inc_gdp,
                                             pred.line.gras2015[[2]], pred.line.di_inc[[2]])
 summary(data_47_MSR$MSR_gras)
 ggplot(data = data_47_MSR) + 
-  geom_histogram(aes(x = MSR_gras), bins = 100) + 
-  xlim(-1, 1)
+  geom_histogram(aes(x = MSR_gras), bins = 100) 
 
 data_47_MSR$MSR_shru <- land.MSR.estimation(data_47_MSR$shru2015, data_47_MSR$di_inc_gdp,
                                             pred.line.shru2015[[2]], pred.line.di_inc[[2]])
 summary(data_47_MSR$MSR_shru)
 ggplot(data = data_47_MSR) + 
-  geom_histogram(aes(x = MSR_shru), bins = 100) + 
-  xlim(-2, 2)
+  geom_histogram(aes(x = MSR_shru), bins = 100) 
 
 data_47_MSR$MSR_wetl <- land.MSR.estimation(data_47_MSR$wetl2015, data_47_MSR$di_inc_gdp,
                                             pred.line.wetl2015[[2]], pred.line.di_inc[[2]])
 summary(data_47_MSR$MSR_wetl)
 ggplot(data = data_47_MSR) + 
-  geom_histogram(aes(x = MSR_wetl), bins = 100) + 
-  xlim(-2, 2)
+  geom_histogram(aes(x = MSR_wetl), bins = 100) 
 
 data_47_MSR$MSR_wetl <- land.MSR.estimation(data_47_MSR$wetl2015, data_47_MSR$di_inc_gdp,
                                             pred.line.wetl2015[[2]], pred.line.di_inc[[2]])
 summary(data_47_MSR$MSR_wetl)
 ggplot(data = data_47_MSR) + 
-  geom_histogram(aes(x = MSR_wetl), bins = 100) + 
-  xlim(-2, 2)
+  geom_histogram(aes(x = MSR_wetl), bins = 100) 
 
 data_47_MSR$MSR_wate <- land.MSR.estimation(data_47_MSR$wate2015, data_47_MSR$di_inc_gdp,
                                             pred.line.wate2015[[2]], pred.line.di_inc[[2]])
 summary(data_47_MSR$MSR_wate)
 ggplot(data = data_47_MSR) + 
-  geom_histogram(aes(x = MSR_wate), bins = 100) + 
-  xlim(-1, 1)
+  geom_histogram(aes(x = MSR_wate), bins = 100) 
 
 data_47_MSR$MSR_impe <- land.MSR.estimation(data_47_MSR$impe2015, data_47_MSR$di_inc_gdp,
                                             pred.line.impe2015[[2]], pred.line.di_inc[[2]])
 summary(data_47_MSR$MSR_impe)
 ggplot(data = data_47_MSR) + 
-  geom_histogram(aes(x = MSR_impe), bins = 100) + 
-  xlim(-1, 1)
+  geom_histogram(aes(x = MSR_impe), bins = 100) 
+
+
+save(data_47_MSR, file = "04_Results/05_MSR_landcover.RData", version = 2)
 
 ### test log function with di_inc_gdp
 pdp.result.di_inc.test <- pdp.result.di_inc
