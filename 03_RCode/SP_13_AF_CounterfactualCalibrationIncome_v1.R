8b9026c795cdcdca4af72577c4e94502a6750b13
Chao Li <chaoli0394@gmail.com>
2022-09-01 20:17:05 +0900

    C: send to HPC; revise SP_14_AN; done

---
+++ 03_RCode/SP_13_AF_CounterfactualCalibrationIncome_v1.R
@@ -0,0 +1,76 @@
+# Author: M.L.
+
+# Aim: this script is to calculation monetary value of counterfactual
+
+# end
+
+library(randomForest)
+library(tidyverse)
+library(dplyr)
+library(foreach)
+library(doParallel)
+library(doSNOW)
+
+dataPre <- function(data){
+  X <- data %>% dplyr::select(-GHQ12, -weights)
+  return(X)
+}
+
+rfPredictYchangeFeature <- function(dataRF, modelRF, landCoverName, marginalChange){
+  dataRF[,landCoverName] <- dataRF[,landCoverName] + marginalChange
+  y_inc = predict(modelRF, newdata = dataRF)
+  return(y_inc)
+}
+
+### this function find the reasonable income value to satisfy the counterfactural
+singleCounterfactual <- function(singleDataRF, modelRF, aimY, tolerance, incomeName,
+                                 incomeChangeLimit, incomeAccuracy){
+  lineNumber <- incomeChangeLimit/incomeAccuracy * 2 + 1
+  halfLineNumber <- incomeChangeLimit/incomeAccuracy
+  searchingDataRF <- singleDataRF[rep(1, lineNumber),]
+  changeValue <- seq(-incomeChangeLimit, incomeChangeLimit, incomeAccuracy)
+  searchingDataRF[,incomeName] <- searchingDataRF[,incomeName] + changeValue
+  y_inc = predict(modelRF, newdata = searchingDataRF)
+  yChange <- abs(y_inc - aimY)
+  resultTable <- as.data.frame(cbind(seq(-halfLineNumber, halfLineNumber, 1), yChange))
+  resultTable <- resultTable[resultTable$yChange<0.1,]
+  if(nrow(resultTable) > 0){
+    resultTable$direction <- resultTable[,1]/abs(resultTable[,1])
+    resultTable[is.na(resultTable[,3]),3] <- 1
+    resultTable[,1] <- abs(resultTable[,1])
+    direction <- resultTable[resultTable[,1]==min(resultTable[,1]),3]
+    returnValue <- min(resultTable[,1]) * direction * incomeAccuracy
+    if(length(returnValue)>1){
+      returnValue <- returnValue[1]
+    }
+  } else {
+    returnValue <- NA
+  }
+  return(returnValue)
+}
+
+progress_fun <- function(n){
+  cat(n, ' ')
+  if (n%%100==0){
+    cat('\n')
+  }
+}
+
+# parallel running the singleCounterfactual
+aggregateCounterfactual <- function(dataRF, modelRF, landCoverName, marginalChange, 
+                                    tolerance, incomeName, incomeChangeLimit, 
+                                    incomeAccuracy, clusterNumber){
+  y_inc <- rfPredictYchangeFeature(dataRF, modelRF, landCoverName, marginalChange)
+  cl <- makeSOCKcluster(clusterNumber)
+  clusterExport(cl, "singleCounterfactual")
+  registerDoSNOW(cl)
+  opts <- list(progress=progress_fun)
+  df.ouput <-
+    foreach(i = seq(1,nrow(dataRF), 1), .combine = 'c', 
+            .packages='randomForest', .options.snow=opts) %dopar% {
+              singleCounterfactual(dataRF[i,], modelRF, y_inc[i], tolerance, 
+                                   incomeName, incomeChangeLimit, incomeAccuracy)
+            }
+  stopCluster(cl)
+  return(df.ouput)
+}
---
+++ 03_RCode/SP_14_AN_CounterfactualValue_v1.R
@@ -0,0 +1,83 @@
+# Author: M.L.
+
+# Aim: this script is to calculation monetary value of counterfactual
+# on HPC
+
+# end
+
+library(randomForest)
+library(tidyverse)
+library(dplyr)
+library(foreach)
+library(doParallel)
+library(doSNOW)
+
+source("DP02/03_RCode/SP_13_AF_CounterfactualCalibrationIncome_v1.R")
+
+load("DP02/02_Data/SP_Data_49Variable_Weights_changeRangeOfLandCover.RData")
+load("DP02/04_results/10_RFresult_49var_weighted.RData")
+X <- dataPre(data_49)
+
+notHave <- F
+if(notHave){
+  counterfactualValueOfCropChange1 <-
+    aggregateCounterfactual(X, data.rf.49.weighted, "crop2015",marginalChange = 1,
+                            0.01, "di_inc_gdp", 1, 10^-3, 20)
+  save(counterfactualValueOfCropChange1, file = "DP02/04_Results/97_temp_cropCounterfactualValue.Rdata")
+}
+
+notHave <- F
+if(notHave){
+  counterfactualValueOfForeChange1 <-
+    aggregateCounterfactual(X, data.rf.49.weighted, "fore2015",marginalChange = 1,
+                            0.01, "di_inc_gdp", 1, 10^-3, 20)
+  save(counterfactualValueOfForeChange1, file = "DP02/04_Results/97_temp_foreCounterfactualValue.Rdata")
+}
+
+notHave <- F
+if(notHave){
+  counterfactualValueOfForeChange1 <-
+    aggregateCounterfactual(X, data.rf.49.weighted, "gras2015",marginalChange = 1,
+                            0.01, "di_inc_gdp", 1, 10^-3, 20)
+  save(counterfactualValueOfForeChange1, file = "DP02/04_Results/97_temp_graCounterfactualValue.Rdata")
+}
+
+notHave <- F
+if(notHave){
+  counterfactualValueOfForeChange1 <-
+    aggregateCounterfactual(X, data.rf.49.weighted, "shru2015",marginalChange = 1,
+                            0.01, "di_inc_gdp", 1, 10^-3, 20)
+  save(counterfactualValueOfForeChange1, file = "DP02/04_Results/97_temp_shruCounterfactualValue.Rdata")
+}
+
+notHave <- F
+if(notHave){
+  counterfactualValueOfForeChange1 <-
+    aggregateCounterfactual(X, data.rf.49.weighted, "wetl2015",marginalChange = 1,
+                            0.01, "di_inc_gdp", 1, 10^-3, 20)
+  save(counterfactualValueOfForeChange1, file = "DP02/04_Results/97_temp_wetlCounterfactualValue.Rdata")
+}
+
+notHave <- F
+if(notHave){
+  counterfactualValueOfForeChange1 <-
+    aggregateCounterfactual(X, data.rf.49.weighted, "wate2015",marginalChange = 1,
+                            0.01, "di_inc_gdp", 1, 10^-3, 20)
+  save(counterfactualValueOfForeChange1, file = "DP02/04_Results/97_temp_wateCounterfactualValue.Rdata")
+}
+
+notHave <- F
+if(notHave){
+  counterfactualValueOfForeChange1 <-
+    aggregateCounterfactual(X, data.rf.49.weighted, "impe2015",marginalChange = 1,
+                            0.01, "di_inc_gdp", 1, 10^-3, 20)
+  save(counterfactualValueOfForeChange1, file = "DP02/04_Results/97_temp_impeCounterfactualValue.Rdata")
+}
+
+notHave <- F
+if(notHave){
+  counterfactualValueOfForeChange1 <-
+    aggregateCounterfactual(X, data.rf.49.weighted, "bare2015",marginalChange = 1,
+                            0.01, "di_inc_gdp", 1, 10^-3, 20)
+  save(counterfactualValueOfForeChange1, file = "DP02/04_Results/97_temp_bareCounterfactualValue.Rdata")
+}
