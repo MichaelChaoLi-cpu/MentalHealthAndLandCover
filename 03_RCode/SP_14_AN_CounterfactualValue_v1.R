a65feecd6a29ad95e615767bfa9956aa77ca64c3
Chao Li <chaoli0394@gmail.com>
2022-09-02 10:07:57 +0900

    O: get data, but variable used in wrong names; redo; done

--- 03_RCode/SP_14_AN_CounterfactualValue_v1.R
+++ 03_RCode/SP_14_AN_CounterfactualValue_v1.R
@@ -36,48 +36,48 @@ if(notHave){
 
 notHave <- F
 if(notHave){
-  counterfactualValueOfForeChange1 <-
+  counterfactualValueOfGrasChange1 <-
     aggregateCounterfactual(X, data.rf.49.weighted, "gras2015",marginalChange = 1,
                             0.01, "di_inc_gdp", 1, 10^-3, 20)
-  save(counterfactualValueOfForeChange1, file = "DP02/04_Results/97_temp_graCounterfactualValue.Rdata")
+  save(counterfactualValueOfGrasChange1, file = "DP02/04_Results/97_temp_grasCounterfactualValue.Rdata")
 }
 
 notHave <- F
 if(notHave){
-  counterfactualValueOfForeChange1 <-
-    aggregateCounterfactual(X, data.rf.49.weighted, "shru2015",marginalChange = 1,
+  counterfactualValueOfShruChange1 <-
+    aggregateCounterfactual(X, data.rf.49.weighted, "shru2015",marginalChange = 0.4,
                             0.01, "di_inc_gdp", 1, 10^-3, 20)
-  save(counterfactualValueOfForeChange1, file = "DP02/04_Results/97_temp_shruCounterfactualValue.Rdata")
+  save(counterfactualValueOfShruChange1, file = "DP02/04_Results/97_temp_shruCounterfactualValue.Rdata")
 }
 
 notHave <- F
 if(notHave){
-  counterfactualValueOfForeChange1 <-
-    aggregateCounterfactual(X, data.rf.49.weighted, "wetl2015",marginalChange = 1,
+  counterfactualValueOfWetlChange1 <-
+    aggregateCounterfactual(X, data.rf.49.weighted, "wetl2015",marginalChange = 0.03,
                             0.01, "di_inc_gdp", 1, 10^-3, 20)
-  save(counterfactualValueOfForeChange1, file = "DP02/04_Results/97_temp_wetlCounterfactualValue.Rdata")
+  save(counterfactualValueOfWetlChange1, file = "DP02/04_Results/97_temp_wetlCounterfactualValue.Rdata")
 }
 
 notHave <- F
 if(notHave){
-  counterfactualValueOfForeChange1 <-
-    aggregateCounterfactual(X, data.rf.49.weighted, "wate2015",marginalChange = 1,
+  counterfactualValueOfWateChange1 <-
+    aggregateCounterfactual(X, data.rf.49.weighted, "wate2015",marginalChange = 0.6,
                             0.01, "di_inc_gdp", 1, 10^-3, 20)
-  save(counterfactualValueOfForeChange1, file = "DP02/04_Results/97_temp_wateCounterfactualValue.Rdata")
+  save(counterfactualValueOfWateChange1, file = "DP02/04_Results/97_temp_wateCounterfactualValue.Rdata")
 }
 
 notHave <- F
 if(notHave){
-  counterfactualValueOfForeChange1 <-
+  counterfactualValueOfImpeChange1 <-
     aggregateCounterfactual(X, data.rf.49.weighted, "impe2015",marginalChange = 1,
                             0.01, "di_inc_gdp", 1, 10^-3, 20)
-  save(counterfactualValueOfForeChange1, file = "DP02/04_Results/97_temp_impeCounterfactualValue.Rdata")
+  save(counterfactualValueOfImpeChange1, file = "DP02/04_Results/97_temp_impeCounterfactualValue.Rdata")
 }
 
 notHave <- F
 if(notHave){
-  counterfactualValueOfForeChange1 <-
-    aggregateCounterfactual(X, data.rf.49.weighted, "bare2015",marginalChange = 1,
+  counterfactualValueOfBareChange1 <-
+    aggregateCounterfactual(X, data.rf.49.weighted, "bare2015",marginalChange = 0.2,
                             0.01, "di_inc_gdp", 1, 10^-3, 20)
-  save(counterfactualValueOfForeChange1, file = "DP02/04_Results/97_temp_bareCounterfactualValue.Rdata")
+  save(counterfactualValueOfBareChange1, file = "DP02/04_Results/97_temp_bareCounterfactualValue.Rdata")
 }
