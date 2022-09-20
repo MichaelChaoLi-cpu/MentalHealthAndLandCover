f5f4062560b5aeb60dc344cd5d0e57d4137cad1d
Chao Li <chaoli0394@gmail.com>
2022-09-20 10:12:30 +0900

    O: revise use the land cover change to offset the change of income; revise 14_AN; done

--- 03_RCode/SP_14_AN_CounterfactualValue_v1.R
+++ 03_RCode/SP_14_AN_CounterfactualValue_v1.R
@@ -81,3 +81,40 @@ if(notHave){
                             0.01, "di_inc_gdp", 1, 10^-3, 20)
   save(counterfactualValueOfBareChange1, file = "DP02/04_Results/97_temp_bareCounterfactualValue.Rdata")
 }
+
+#### use land cover change to offset the change of income
+notHave <- T
+if(notHave){
+  counterfactualValueOfCropChangeReversal0.01 <-
+    aggregateCounterfactual(X, data.rf.49.weighted, "di_inc_gdp",marginalChange = 0.01,
+                            0.01, "crop2015", 10, 10^-3, 20)
+  save(counterfactualValueOfCropChangeReversal0.01, file = "DP02/04_Results/96_test_counterfactualValueOfCropChangeReversal0.01.Rdata")
+  counterfactualValueOfForeChangeReversal0.01 <-
+    aggregateCounterfactual(X, data.rf.49.weighted, "di_inc_gdp",marginalChange = 0.01,
+                            0.01, "fore2015", 10, 10^-3, 20)
+  save(counterfactualValueOfForeChangeReversal0.01, file = "DP02/04_Results/96_test_counterfactualValueOfForeChangeReversal0.01.Rdata") 
+  counterfactualValueOfGrasChangeReversal0.01 <-
+    aggregateCounterfactual(X, data.rf.49.weighted, "di_inc_gdp",marginalChange = 0.01,
+                            0.01, "gras2015", 10, 10^-3, 20)
+  save(counterfactualValueOfGrasChangeReversal0.01, file = "DP02/04_Results/96_test_counterfactualValueOfGrasChangeReversal0.01.Rdata")
+  counterfactualValueOfShruChangeReversal0.01 <-
+    aggregateCounterfactual(X, data.rf.49.weighted, "di_inc_gdp",marginalChange = 0.01,
+                            0.01, "shru2015", 10, 10^-3, 20)
+  save(counterfactualValueOfShruChangeReversal0.01, file = "DP02/04_Results/96_test_counterfactualValueOfShruChangeReversal0.01.Rdata")
+  counterfactualValueOfWetlChangeReversal0.01 <-
+    aggregateCounterfactual(X, data.rf.49.weighted, "di_inc_gdp",marginalChange = 0.01,
+                            0.01, "wetl2015", 10, 10^-3, 20)
+  save(counterfactualValueOfWetlChangeReversal0.01, file = "DP02/04_Results/96_test_counterfactualValueOfWetlChangeReversal0.01.Rdata")
+  counterfactualValueOfWateChangeReversal0.01 <-
+    aggregateCounterfactual(X, data.rf.49.weighted, "di_inc_gdp",marginalChange = 0.01,
+                            0.01, "wate2015", 10, 10^-3, 20)
+  save(counterfactualValueOfWateChangeReversal0.01, file = "DP02/04_Results/96_test_counterfactualValueOfWateChangeReversal0.01.Rdata")
+  counterfactualValueOfImpeChangeReversal0.01 <-
+    aggregateCounterfactual(X, data.rf.49.weighted, "di_inc_gdp",marginalChange = 0.01,
+                            0.01, "impe2015", 10, 10^-3, 20)
+  save(counterfactualValueOfImpeChangeReversal0.01, file = "DP02/04_Results/96_test_counterfactualValueOfImpeChangeReversal0.01.Rdata")
+  counterfactualValueOfBareChangeReversal0.01 <-
+    aggregateCounterfactual(X, data.rf.49.weighted, "di_inc_gdp",marginalChange = 0.01,
+                            0.01, "bare2015", 10, 10^-3, 20)
+  save(counterfactualValueOfBareChangeReversal0.01, file = "DP02/04_Results/96_test_counterfactualValueOfBareChangeReversal0.01.Rdata")
+}
