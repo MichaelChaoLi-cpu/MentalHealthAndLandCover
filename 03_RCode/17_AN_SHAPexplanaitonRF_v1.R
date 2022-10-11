c589e55f525b49e790e78846ca960f78f49d3e69
Chao Li <chaoli0394@gmail.com>
2022-10-11 15:19:41 +0900

    B: prepare to set a version to HPC; revise 17_AN; done

--- 03_RCode/17_AN_SHAPexplanaitonRF_v1.R
+++ 03_RCode/17_AN_SHAPexplanaitonRF_v1.R
@@ -21,13 +21,27 @@ load("02_Data/SP_Data_49Variable_Weights_changeRangeOfLandCover.RData")
 load("04_results/10_RFresult_49var_weighted.RData")
 X <- dataPre(data_49)
 
-test1 <- data_49[1,]
 explain_rf <- DALEX::explain(model = data.rf.49.weighted,  
                              data = X,
                              y = data_49$GHQ12, 
                              label = "Random Forest")
+
+test1 <- data_49[1,]
 shap_test1 <- DALEX::predict_parts(
   explainer = explain_rf, 
   new_observation = test1,
-  type = "shap",
-  B = 1000)
+  type = "shap")
+
+shap_test1.interest <- shap_test1 %>% filter(
+  variable_name %in% c("bare2015", "crop2015", "fore2015", "gras2015",
+                    "impe2015", "shru2015", "wate2015", "wetl2015", "di_inc_gdp" )
+)
+
+plot(shap_test1.interest)
+
+shap_test2 <- DALEX::predict_parts(
+  explainer = explain_rf, 
+  new_observation = test1,
+  N = 10000,
+  type = "shap")
+
