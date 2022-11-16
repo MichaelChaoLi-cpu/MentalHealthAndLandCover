2e88e73a8dcf47777e7295b10ef0b3cacac64e09
Chao Li <chaoli0394@gmail.com>
2022-11-16 11:28:10 +0900

    C: upload some visual; revise 18_DW; done

--- 03_RCode/18_DW_49VariableRdsToPython_v1.R
+++ 03_RCode/18_DW_49VariableRdsToPython_v1.R
@@ -10,3 +10,67 @@ library(dplyr)
 load(file = "02_Data/SP_Data_49Variable_Weights_changeRangeOfLandCover.RData")
 
 saveRDS(data_49, file = "02_Data/SP_Data_49Variable_Weights_changeRangeOfLandCover_RdsVer.Rds", version = 2)
+
+
+load("01_PrivateData/01_Dataset.RData")
+load("01_PrivateData/02_coordOfSID.RData")
+
+coordDF$StandardID <- coordDF$StandardID %>% as.numeric() 
+colnames(coordDF)[1] <- "ID"
+coordDF <- coordDF %>% as.tibble()
+
+data <- left_join(data, coordDF)
+data_t <- data %>%
+  dplyr::select(over_LS, country, ind_income_usd, di_inc_gdp, weather, trust, trusted, social_class, income_group,
+                female_dummy, age, GHQ12, GHQ12_1:GHQ12_12, sr_health, student:unemployed, bachelor:phd,
+                pleasure_all, anger_all, sadness_all, enjoyment_all, smile_all,
+                pleasure_wk, anger_wk, sadness_wk, enjoyment_wk, smile_wk,
+                com_livable, com_attach, com_satety, partner_dummy, 
+                child_num, child_u6_num, family_mem, im_relat_fami:im_object_media, dis_space_home:uncreative,
+                prec:snow2015, X, Y, ID)
+
+### we build the dataset with 49 predictors compare to data_47 we add X and Y
+### since residuals are correlated with y, so we need to add the weights to solve this problem.
+data_49 <- data_t %>% dplyr::select(GHQ12, di_inc_gdp, social_class,student:unemployed,
+                                    pleasure_all:smile_all,
+                                    euthusiastic:uncreative, urban_cent:rural_area,
+                                    income_group, female_dummy, age, sr_health, bachelor:phd,
+                                    com_livable:com_satety, child_num, crop2015:wate2015, impe2015,
+                                    bare2015, X, Y, country) %>%
+  na.omit()
+
+GHQ12_count <- data_49 %>% count(GHQ12)
+GHQ12_count$weights <- 1/(GHQ12_count$n/max(GHQ12_count$n))
+GHQ12_count <- GHQ12_count %>% dplyr::select(GHQ12, weights)
+data_49 <- left_join(data_49, GHQ12_count)
+
+### data correction
+data_49 <- data_49 %>%
+  mutate(di_inc_gdp = ifelse(di_inc_gdp > 3, 3, di_inc_gdp))
+#here, we think the indiviudal income = 400% gdp per capita is very high
+hist(data_49$shru2015, breaks = seq(0, 100, by = 0.05), ylim = c(0, 20)) # 0 - 40
+data_49 <- data_49 %>%
+  mutate(shru2015 = ifelse(shru2015 > 40, 40, shru2015))
+#here, according to distribution of shru2015, we cut shru2015 to 40
+hist(data_49$wetl2015, breaks = seq(0, 100, by = 0.05), ylim = c(0, 10), xlim = c(0, 5)) # 0 - 3 
+data_49 <- data_49 %>%
+  mutate(wetl2015 = ifelse(wetl2015 > 3, 3, wetl2015))
+#here, according to distribution of wetl2015, we cut wetl2015 to 3
+hist(data_49$wate2015, breaks = seq(0, 60, by = 0.05), ylim = c(0, 20)) #0 - 50
+data_49 <- data_49 %>%
+  mutate(wate2015 = ifelse(wate2015 > 50, 50, wate2015))
+#here, according to distribution of wate2015, we cut wate2015 to 60
+hist(data_49$bare2015, breaks = seq(0, 100, by = 0.05), ylim = c(0, 20)) #0 - 20
+data_49 <- data_49 %>%
+  mutate(bare2015 = ifelse(bare2015 > 20, 20, bare2015))
+levels(data_49$country) <- c("Japan", 'Thailand', "Malaysia", "Indonesia", "Singapore",
+                             "Vietnam", "Philippines", "Mexico", "Venezuela", "Chile",
+                             "Brazile", "Colombia", 'South Africa', "India", "Myanmar",
+                             "Kazakhstan", "Mongolia", "Egypt", "Russia", "China",
+                             "Australia", "United States", "Germany", "United Kingdom",
+                             "France", "Spain", "Italy", "Sweden", "Canada",
+                             "Netherlands", "Greece", "Turkey", "Hungary", "Poland",
+                             "Czech", "Romania", "Sri Lanka")
+data_49$country <- data_49$country %>% as.character()
+saveRDS(data_49, file = "02_Data/SP_Data_49Variable_with_country.Rds", version = 2)
+
--- 07_PyCode/08_VI_PlotFigureInManu_v0.py
+++ 07_PyCode/08_VI_PlotFigureInManu_v0.py
@@ -12,6 +12,10 @@ import mpl_scatter_density # adds projection='scatter_density'
 import matplotlib.colors
 import matplotlib as mpl
 import plotly.express as px
+import pyreadr
+from sklearn.inspection import permutation_importance
+### Note: permutation_importance is more suitbale to analysis, rather than 
+###       model.feature_importance .
 
 DP02_FIGURE_LOCATION = "D:/OneDrive - Kyushu University/02_Article/03_RStudio/05_Figure/"
 CMAP = matplotlib.colors.LinearSegmentedColormap.from_list("", ["blue","green","yellow","red"])
@@ -85,14 +89,99 @@ def getYwithCountry():
     return X
 
 def drawGhqHist(X, figure_name):
+    color_array = [
+        '#2E91E5', '#E15F99', '#1CA71C', '#FB0D0D', '#DA16FF', '#222A2A',
+        '#B68100', '#750D86', '#EB663B', '#511CFB', '#00A08B', '#FB00D1',
+        '#FC0080', '#B2828D', '#6C7C32', '#778AAE', '#862A16', '#A777F1',
+        '#620042', '#1616A7', '#DA60CA', '#6C4516', '#0D2A63', '#AF0038',
+        '#FD3216', '#00FE35', '#6A76FC', '#FED4C4', '#FE00CE', '#0DF9FF',
+        '#F6F926', '#FF9616', '#479B55', '#EEA6FB', '#DC587D', '#D626FF',
+        '#6E899C'
+        ]
     plt.rcParams["figure.dpi"] = 1000
-    ax = X.plot.bar(stacked=True, figsize=(21, 14), color = px.colors.qualitative.Alphabet)
+    ax = X.plot.bar(stacked=True, figsize=(21, 14), color = color_array)
     #ax.bar(X.index, X['Japan'], label='Japan', bottom=X['Australia'])
     ax.grid(True)
     ax.set_ylabel("Counts", fontsize=25)
+    ax.text(25, 9600, "N = 89,273", fontsize=25)
+    ax.text(25, 9200, "Mean = 24.3185", fontsize=25)
+    ax.text(25, 8800, "Standard Deviation = 6.301", fontsize=25)
+    ax.text(25, 8400, "Median = 25", fontsize=25)
     ax.legend(fontsize=20, ncol=3)
     ax.set_xlabel("Mental Health Score (GHQ-12)", fontsize=25)
     ax.tick_params(axis='both', which='major', labelsize=20)
+    plt.savefig(DP02_FIGURE_LOCATION + figure_name)
+
+def getIncWithCountry():
+    DP02_location = "D:/OneDrive - Kyushu University/02_Article/03_RStudio/"
+    ### X and y
+    dataset = pyreadr.read_r(DP02_location + "02_Data/SP_Data_49Variable_with_country.Rds")
+    dataset = dataset[None]
+    X = dataset[['di_inc_gdp', 'country']]
+    X['di_inc_gdp'] = round(X['di_inc_gdp'], 1)
+    X = X.groupby(['country'])['di_inc_gdp'].value_counts().unstack("country",
+                                                               fill_value=0)
+    return X
+
+def drawDigHist(X, figure_name):
+    color_array = [
+        '#2E91E5', '#E15F99', '#1CA71C', '#FB0D0D', '#DA16FF', '#222A2A',
+        '#B68100', '#750D86', '#EB663B', '#511CFB', '#00A08B', '#FB00D1',
+        '#FC0080', '#B2828D', '#6C7C32', '#778AAE', '#862A16', '#A777F1',
+        '#620042', '#1616A7', '#DA60CA', '#6C4516', '#0D2A63', '#AF0038',
+        '#FD3216', '#00FE35', '#6A76FC', '#FED4C4', '#FE00CE', '#0DF9FF',
+        '#F6F926', '#FF9616', '#479B55', '#EEA6FB', '#DC587D', '#D626FF',
+        '#6E899C'
+        ]
+    plt.rcParams["figure.dpi"] = 1000
+    ax = X.plot.bar(stacked=True, figsize=(21, 14), color = color_array)
+    #ax.bar(X.index, X['Japan'], label='Japan', bottom=X['Australia'])
+    ax.grid(True)
+    ax.set_ylabel("Counts", fontsize=25)
+    ax.text(25, 5200, "N = 89,273", fontsize=25)
+    ax.text(25, 4800, "Mean = -0.3165", fontsize=25)
+    ax.text(25, 4400, "Standard Deviation = 0.7195", fontsize=25)
+    ax.text(25, 4000, "Median = -0.5299", fontsize=25)
+    ax.legend(fontsize=20, ncol=3)
+    ax.set_xlabel("DIG", fontsize=25)
+    ax.tick_params(axis='both', which='major', labelsize=20)
+    plt.savefig(DP02_FIGURE_LOCATION + figure_name)
+
+def getImportance():
+    DP02_location = "D:/OneDrive - Kyushu University/02_Article/03_RStudio/"
+    dataset = pyreadr.read_r(DP02_location + "02_Data/SP_Data_49Variable_Weights_changeRangeOfLandCover_RdsVer.Rds")
+    dataset = dataset[None]
+    y = np.array(dataset.iloc[:, 0:1].values.flatten(), dtype='float64')
+    X = np.array(dataset.iloc[:, 1:50], dtype='float64')
+    model = RandomForestRegressor(n_estimators=1000, oob_score=True, 
+                                   random_state=1, max_features = 9, n_jobs=-1)
+    model.fit(X, y)
+    result = permutation_importance(model, X, y, n_repeats=10, random_state=1)
+    return result
+
+def drawImportanceBar(X, figure_name):
+    fig, axs = plt.subplots(figsize=(30, 20), dpi=1000)
+    feature_name = ["DIG", "Social Class", "Student", "Worker", "Company Owner", 
+                    "Government Officer", "Self-Employed", "Professional Job",
+                    "Housewife", "Unemployed", "Pleasure", "Anger", "Sadness", 
+                    "Enjoyment", "Smile", "Enthusiastic", "Critical", "Dependable",
+                    "Anxious", "Open to New Experience", "Reserved", "Sympathetic",
+                    "Careless", "Calm", "Uncreative", "Urban Center Dummy", 
+                    "Urban Area Dummy", "Rural Area Dummy", "Income Group", 
+                    "Female", "Age", "Self-reported Health", "Bachelor",
+                    "Master", "PhD", "Community Livable", "Community Attachment",
+                    "Community Safety", "Children Number", "Cropland (%)", "Forest (%)",
+                    "Grassland (%)", "Shrubland (%)", "Wetland (%)", "Water (%)", 
+                    "Urban Land (%)", "Bare Land (%)", "Longitude", "Latitude"]
+    y_pos = np.arange(len(feature_name))
+    axs.barh(y_pos, X, align='center')
+    axs.set_yticks(y_pos, labels=feature_name)
+    axs.invert_yaxis() 
+    axs.set_xlabel('Gini Importance', fontsize=25)
+    axs.set_title('Feuture Importance', fontsize=25)
+    axs.grid(True)
+    axs.tick_params(axis='both', which='major', labelsize=20)
+    axs.set_ylim([49, -1])
     plt.savefig(DP02_FIGURE_LOCATION + figure_name)
     
 
@@ -101,3 +190,9 @@ drawYandY_pred(y, y_pred, "y_yhat.jpg")
 
 X = getYwithCountry()
 drawGhqHist(X, "GHQhist.jpg")
+
+X = getIncWithCountry()
+drawDigHist(X, "DIGhist.jpg")
+
+featureImportance = getImportance()
+drawImportanceBar(featureImportance, "FeatureImportance.jpg")
